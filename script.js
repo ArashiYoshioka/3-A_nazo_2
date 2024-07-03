bc96a43c874368432096f5172b9a1ad476edb8ed
ArashiYoshioka <220808a34@hokkaido-c.ed.jp>
2024-07-04 03:28:43 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function checkAnswer() {
+    const answerInput = document.getElementById('answerInput').value;
+    const result = document.getElementById('result');
+
+    if (answerInput === "フラスコ") {
+        result.textContent = "おめでとう";
+    } else {
+        result.textContent = "残念";
+    }
+}
